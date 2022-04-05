9e200bb5c0abb53431c8d34e5f85cef39a89d70d
mnsbzk <mnsbzk0603@gmail.com>
2022-04-05 12:09:44 +0900

    CRUD処理、一覧表示機能の追加

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Requests\QuizRequest;
 
 class QuizController extends Controller
 {
@@ -14,15 +15,50 @@ class QuizController extends Controller
         $this->question = $question;
     }
 
+    public function index()
+    {
+        $questions = $this->question->all();
+        return view('quiz.index', ['questions' => $questions]);
+    }
+
     public function create()
     {
         return view('quiz.create');
     }
 
-    public function store()
+    public function store(QuizRequest $request)
     {
         $inputs = $request->all();
-        $this->quiz->fill($inputs);
-        $this->quiz->save();
+        $this->question->fill($inputs);
+        $this->question->save();
+        return redirect()->route('quiz.index');
+    }
+
+    public function show($id)
+    {
+        $question = $this->question->find($id);
+        return view('quiz.show', ['question' => $question]);
+    }
+
+    public function edit($id)
+    {
+        $question = $this->question->find($id);
+        return view('quiz.edit', ['question' => $question]);
+    }
+
+    public function update(QuizRequest $request, $id)
+    {
+        $inputs = $request->all();
+        $question = $this->question->find($id);
+        $question->fill($inputs);
+        $question->save();
+        return redirect()->route('quiz.show', $question->id);
+    }
+
+    public function delete($id)
+    {
+        $question = $this->question->find($id);
+        $question->delete();
+        return redirect()->route('quiz.index');
     }
 }
