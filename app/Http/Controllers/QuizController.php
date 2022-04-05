6c76cddc2dc6a014fb44081c2dcb0379cb05bf68
mnsbzk <mnsbzk0603@gmail.com>
2022-04-05 19:06:38 +0900

    選択肢新規登録処理、選択肢一覧表示作成、choiceモデルのコンストラクタインジェクション 追加

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Question;
+use App\Choice;
 use Illuminate\Http\Request;
 use App\Http\Requests\QuizRequest;
+use App\Http\Requests\ChoiceRequest;
 
 class QuizController extends Controller
 {
     private $question;
+    private $choice;
 
-    public function __construct(Question $question)
+    public function __construct(Question $question, Choice $choice)
     {
         $this->question = $question;
+        $this->choice = $choice;
     }
 
     // 問題一覧表示の為のデータ取得
@@ -67,5 +71,29 @@ class QuizController extends Controller
         $question = $this->question->find($id);
         $question->delete();
         return redirect()->route('quiz.index');
+    }
+
+    // 選択肢新規作成処理
+    public function StoreChoice(ChoiceRequest $request,$QuestionId)
+    {
+        $inputs = $request->all();
+        $inputs += array('question_id'=>$QuestionId);
+        if(array_key_exists('is_correct',$inputs) === false){
+            $inputs += array('is_correct'=>'0');
+        } else {
+            $inputs['is_correct'] = '1';
+        }
+        $this->choice->fill($inputs);
+        $this->choice->save();
+        return redirect()->route('quiz.show', $QuestionId);
+    }
+
+    // 選択肢一覧取得
+    public function IndexChoice($QuestionId)
+    {
+        $choices = $this->choice->all($QuestionId);
+        $question = $this->question->find($QuestionId);
+        dd($choices);
+        return view('quiz.show', ['choices' => $choices,'question' => $question]);
     }
 }
