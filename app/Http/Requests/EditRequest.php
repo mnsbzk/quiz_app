f3295fe096b854cb1df9f2ca2c4b5150c3906b10
mnsbzk <mnsbzk0603@gmail.com>
2022-04-06 16:49:09 +0900

    更新時バリデーションファイル作成

---
+++ app/Http/Requests/EditRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'question_text' => 'required|max:255',
+            'choice_text' => 'required|max:255',
+            'is_correct' => 'required|array'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'question_text.required' => '問題が入力されていません。',
+            'question_text.max' => '問題は :max 文字以内で入力してください。',
+            'choice_text.required' => '問題が入力されていません。',
+            'choice_text.max' => '問題は :max 文字以内で入力してください。',
+            'is_correct.required' => '正解の選択肢がありません。'
+        ];
+    }
+}
