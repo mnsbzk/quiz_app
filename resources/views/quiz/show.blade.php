817e7db917debe622955d01cabc3d4db996f95fd
mnsbzk <mnsbzk0603@gmail.com>
2022-04-06 18:55:05 +0900

    問題詳細画面一部修正

--- resources/views/quiz/show.blade.php
+++ resources/views/quiz/show.blade.php
@@ -24,29 +24,34 @@
       </div>
     </div>
     <!-- 選択肢一覧 -->
-    <div>
-      @foreach ($choices as $choice)
-      <div class="list-group-item d-flex justify-content-around">
-        @if($choice->is_correct)
-          <p>正解</p>
-        @else
-          <p>不正解</p>
-        @endif
-        <p>{{ $choice->choice_text }}</p>
-        <form method="POST" action="{{ route('choice.delete', $choice->id) }}">
-          @method('delete')
-          @csrf
-          <button type="submit" class="btn btn-danger">削除する</button>
-        </form>
+    <div class="card-header">
+        選択肢
+      <div class="choice_show">
+        @foreach ($choices as $choice)
+          <div class="list-group-item d-flex justify-content-around">
+            @if($choice->is_correct)
+              <p>正解</p>
+            @else
+              <p>不正解</p>
+            @endif
+            <p>{{ $choice->choice_text }}</p>
+            <form method="POST" action="{{ route('choice.delete', $choice->id) }}">
+              @method('delete')
+              @csrf
+              <button type="submit" class="btn btn-danger">削除する</button>
+            </form>
+          </div>
+        @endforeach
       </div>
-      @endforeach
     </div>
     <!-- 選択肢登録フォーム -->
     <div class="">
-      <form method="POST" action="{{ route('choice.store', $question->id) }}" class="d-flex">
+      <form method="POST" action="{{ route('choice.store', $question->id) }}" class="d-flex justify-content-around">
         @csrf
-        <input type="checkbox" class="checkbox" name="is_correct" value="1">
-        <input type="text" class="" name="choice_text" value="">
+        <div>
+          <input type="checkbox" class="checkbox" name="is_correct" value="1">
+          <input type="text" class="" name="choice_text" value="">
+        </div>
         <button type="submit" class="btn btn-success">登録</button>
       </form>
     </div>
