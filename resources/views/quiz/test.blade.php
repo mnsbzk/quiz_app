db1d9d062e559a9f5a6598a704803ceaa8e6ffb6
mnsbzk <mnsbzk0603@gmail.com>
2022-04-06 18:55:30 +0900

    問題解答画面作成

---
+++ resources/views/quiz/test.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.base')
+@section('content')
+<div class="row justify-content-center">
+  <div class="col-md-8">
+    <div class="card">
+      <div class="card-header">
+        問題一覧
+      </div>
+      <div class="list-group list-group-flush">
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
