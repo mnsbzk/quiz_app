c73aeee4e4079b749c0a7f8f55148f317bfef275
mnsbzk <mnsbzk0603@gmail.com>
2022-04-08 19:21:21 +0900

    問題の一覧表示追加

--- resources/views/quiz/test.blade.php
+++ resources/views/quiz/test.blade.php
@@ -1,14 +1,26 @@
 @extends('layouts.base')
 @section('content')
 <div class="row justify-content-center">
-  <div class="col-md-8">
+  <form class="col-md-8" method="POST" action="{{ route('test.answer') }}">
+  @csrf
     <div class="card">
       <div class="card-header">
         問題一覧
       </div>
       <div class="list-group list-group-flush">
+      @foreach ($questions as $index => $question)
+        <input type="hidden" name="question_id[]" value="{{ $question->id }}">
+        <p>
+          Q{{ $index + 1 }}:{{ $question->question_text }}
+        </p>
+        @foreach ($question->choices as $choice)
+          <input type="hidden" name="choice_id[]" value="{{ $choice->id }}">
+          <label for=""><input type="checkbox" name="is_correct_{{$question->id}}[]" value="{{ $choice->id }}">{{ $choice->choice_text }}</label>
+        @endforeach
+      @endforeach
       </div>
     </div>
-  </div>
+    <button type="submit" class="btn btn-primary">採点する</button>
+  </form>
 </div>
 @endsection
